49588dd9d8b5683699d429ed7abc8d928ded87a5
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-01-05 17:29:15 +0000

    adding deployment pipleine

--- frontend/src/services/contactApi.js
+++ frontend/src/services/contactApi.js
@@ -1,33 +1,26 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const baseUrl = '/api/persons'
+const baseUrl = '/api/persons';
 
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
-const getAll = ()=>{
-    const request= axios
-    .get(baseUrl)
-   return request.then(response=>response.data)
-            
-               
-}
+const create = newObj => {
+  const request = axios.post(baseUrl, newObj);
+  return request.then(response => response.data);
+};
 
+const remove = id => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then(response => response.data);
+};
 
-const create = newObj =>{
-    const request = axios.post(baseUrl, newObj)
-    return request.then(response=>response.data)
-               
-}
+const contactApi = {
+  getAll,
+  create,
+  remove,
+};
 
-const remove = id=>{
-    // console.log(id)
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response=>response.data)
-}
-
-
-export default{
-    getAll,
-    create,
-    remove
-
-}
+export default contactApi;
