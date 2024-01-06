5ef3075a2d0c6e643d6dceb98c7dc35cb63fc71b
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-01-06 00:24:31 +0000

    twiking the build step in other for it to work

--- build_step.sh
+++ build_step.sh
@@ -6,5 +6,5 @@ echo "Build script"
 # add the commands here
 # nvm install 14  # Install Node.js version 14 (for example)
 # nvm use 14 
-npm install && cd ./frontend && npm install
-rm -rf build && cd ./frontend && npm run build && cp -r build ..
+npm install && cd ./frontend && npm install && cd .. && rm -rf build && cd ./frontend && npm run build && cp -r build ..
+
